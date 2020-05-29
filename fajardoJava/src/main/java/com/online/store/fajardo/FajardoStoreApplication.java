b1b1efce9d42324ee2a33dc4d8a175bc1ddcf85b
Arlwin <arlwinkarlfajardo@gmail.com>
2020-05-30 00:55:40 +0800

    initial commit

---
+++ fajardoJava/src/main/java/com/online/store/fajardo/FajardoStoreApplication.java
@@ -0,0 +1,13 @@
+package com.online.store.fajardo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FajardoStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FajardoStoreApplication.class, args);
+	}
+
+}
---
+++ fajardoJava/src/test/java/com/online/store/fajardo/FajardoStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online.store.fajardo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FajardoStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
